84fd097b3efa0bfba1fc86410f1ab867edd1d9bf
micele00l <micele00l@gmail.com>
2025-03-30 14:16:58 +0800

    init files

---
+++ components/esp_sparkbot_bsp/include/bsp/esp_sparkbot_bsp.h
@@ -0,0 +1,319 @@
+/*
+ * SPDX-FileCopyrightText: 2022-2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief ESP BSP: ESP-SparkBot
+ */
+
+#pragma once
+
+#include "sdkconfig.h"
+#include "driver/gpio.h"
+#include "driver/sdmmc_host.h"
+#include "esp_codec_dev.h"
+#include "iot_button.h"
+#include "touch_element/touch_button.h"
+#include "bsp/config.h"
+#include "bsp/display.h"
+#include "i2c_bus.h"
+
+#if ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(5, 0, 0)
+#include "driver/i2s.h"
+#else
+#include "driver/i2s_std.h"
+#endif
+
+#if (BSP_CONFIG_NO_GRAPHIC_LIB == 0)
+#include "lvgl.h"
+#include "esp_lvgl_port.h"
+#endif // BSP_CONFIG_NO_GRAPHIC_LIB == 0
+
+/**************************************************************************************************
+ *  BSP Capabilities
+ **************************************************************************************************/
+
+#define BSP_CAPS_DISPLAY        1
+#define BSP_CAPS_TOUCH          0
+#define BSP_CAPS_BUTTONS        1
+#define BSP_CAPS_AUDIO          1
+#define BSP_CAPS_AUDIO_SPEAKER  0
+#define BSP_CAPS_AUDIO_MIC      1
+#define BSP_CAPS_SDCARD         1
+
+/**************************************************************************************************
+ * ESP-SparkBot-BSP pinout
+ **************************************************************************************************/
+
+/* I2C */
+#define BSP_I2C_SCL           (GPIO_NUM_5)
+#define BSP_I2C_SDA           (GPIO_NUM_4)
+
+/* Display */
+#define BSP_LCD_SPI_MOSI      (GPIO_NUM_47)
+#define BSP_LCD_SPI_CLK       (GPIO_NUM_21)
+// #define BSP_LCD_SPI_CS        (GPIO_NUM_44)
+// #define BSP_LCD_DC            (GPIO_NUM_43)
+#define BSP_LCD_SPI_CS        (GPIO_NUM_7)
+#define BSP_LCD_DC            (GPIO_NUM_6)
+#define BSP_LCD_RST           (GPIO_NUM_NC)
+#define BSP_LCD_BACKLIGHT     (GPIO_NUM_46)
+
+
+/* Buttons */
+#define BSP_BUTTON_BOOT_IO   (GPIO_NUM_0)
+#define BSP_BUTTONS_IO       (GPIO_NUM_1) // All 4 buttons mapped to this GPIO
+typedef enum bsp_led_t {
+    BSP_LED_GREEN = GPIO_NUM_3,
+} bsp_led_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**************************************************************************************************
+ *
+ * Buttons interface
+ *
+ * Example configuration:
+ * \code{.c}
+ * button_handle_t btns[BSP_BUTTON_NUM];
+ * bsp_iot_button_create(btns, NULL, BSP_BUTTON_NUM);
+ * iot_button_register_cb(btns[0], ...
+ * \endcode
+ **************************************************************************************************/
+typedef enum {
+    BSP_BUTTON_MENU = 0,
+    BSP_BUTTON_PLAY,
+    BSP_BUTTON_DOWN,
+    BSP_BUTTON_UP,
+    BSP_BUTTON_BOOT,
+    BSP_BUTTON_NUM
+} bsp_button_t;
+
+/**
+ * @brief Initialize all buttons
+ *
+ * Returned button handlers must be used with espressif/button component API
+ *
+ * @note For LCD panel button which is defined as BSP_BUTTON_MAIN, bsp_display_start should
+ *       be called before call this function.
+ *
+ * @param[out] btn_array      Output button array
+ * @param[out] btn_cnt        Number of button handlers saved to btn_array, can be NULL
+ * @param[in]  btn_array_size Size of output button array. Must be at least BSP_BUTTON_NUM
+ * @return
+ *     - ESP_OK               All buttons initialized
+ *     - ESP_ERR_INVALID_ARG  btn_array is too small or NULL
+ *     - ESP_FAIL             Underlaying iot_button_create failed
+ */
+esp_err_t bsp_iot_button_create(button_handle_t btn_array[], int *btn_cnt, int btn_array_size);
+
+
+void bsp_touch_button_create(touch_button_callback_t button_callback);
+
+/**************************************************************************************************
+ *
+ * I2C interface
+ *
+ * There are two devices connected to I2C peripheral:
+ *  - QMA7981 Inertial measurement unit
+ *  - OV2640 Camera module
+ **************************************************************************************************/
+#define BSP_I2C_NUM     CONFIG_BSP_I2C_NUM
+
+/**
+ * @brief Init I2C driver
+ *
+ * @return
+ *      - ESP_OK                On success
+ *      - ESP_ERR_INVALID_ARG   I2C parameter error
+ *      - ESP_FAIL              I2C driver installation error
+ *
+ */
+esp_err_t bsp_i2c_init(void);
+
+/**
+ * @brief Deinit I2C driver and free its resources
+ *
+ * @return
+ *      - ESP_OK                On success
+ *      - ESP_ERR_INVALID_ARG   I2C parameter error
+ *
+ */
+esp_err_t bsp_i2c_deinit(void);
+
+i2c_bus_handle_t bsp_i2c_get_handle(void);
+
+
+
+/**************************************************************************************************
+ *
+ * SPIFFS
+ *
+ * After mounting the SPIFFS, it can be accessed with stdio functions ie.:
+ * \code{.c}
+ * FILE* f = fopen(BSP_SPIFFS_MOUNT_POINT"/hello.txt", "w");
+ * fprintf(f, "Hello World!\n");
+ * fclose(f);
+ * \endcode
+ **************************************************************************************************/
+#define BSP_SPIFFS_MOUNT_POINT      CONFIG_BSP_SPIFFS_MOUNT_POINT
+
+/**
+ * @brief Mount SPIFFS to virtual file system
+ *
+ * @return
+ *      - ESP_OK on success
+ *      - ESP_ERR_INVALID_STATE if esp_vfs_spiffs_register was already called
+ *      - ESP_ERR_NO_MEM if memory can not be allocated
+ *      - ESP_FAIL if partition can not be mounted
+ *      - other error codes
+ */
+esp_err_t bsp_spiffs_mount(void);
+
+/**
+ * @brief Unmount SPIFFS from virtual file system
+ *
+ * @return
+ *      - ESP_OK on success
+ *      - ESP_ERR_INVALID_STATE if already unmounted
+ */
+esp_err_t bsp_spiffs_unmount(void);
+
+
+/**************************************************************************************************
+ *
+ * LCD interface
+ *
+ * ESP-SparkBot-BSP is shipped with 1.3inch ST7789 display controller.
+ * It features 16-bit colors and 240x240 resolution.
+ *
+ * LVGL is used as graphics library. LVGL is NOT thread safe, therefore the user must take LVGL mutex
+ * by calling bsp_display_lock() before calling any LVGL API (lv_...) and then give the mutex with
+ * bsp_display_unlock().
+ *
+ * If you want to use the display without LVGL, see bsp/display.h API and use BSP version with 'noglib' suffix.
+ **************************************************************************************************/
+// #define BSP_LCD_PIXEL_CLOCK_HZ     (80 * 1000 * 1000)
+#define BSP_LCD_PIXEL_CLOCK_HZ     (40 * 1000 * 1000)
+#define BSP_LCD_SPI_NUM            (SPI3_HOST)
+
+#if (BSP_CONFIG_NO_GRAPHIC_LIB == 0)
+#define BSP_LCD_DRAW_BUFF_SIZE     (BSP_LCD_H_RES * BSP_LCD_V_RES)
+// #define BSP_LCD_DRAW_BUFF_DOUBLE   (0)
+#define BSP_LCD_DRAW_BUFF_DOUBLE   (1)
+#define BSP_LCD_DRAW_BUFF_HEIGHT (50)
+#define BSP_LCD_BL_ON_LEVEL (1)
+
+/**
+ * @brief BSP display configuration structure
+ */
+typedef struct {
+    lvgl_port_cfg_t lvgl_port_cfg;  /*!< LVGL port configuration */
+    uint32_t        buffer_size;    /*!< Size of the buffer for the screen in pixels */
+    uint32_t        trans_size;
+    bool            double_buffer;  /*!< True, if should be allocated two buffers */
+    struct {
+        unsigned int buff_dma: 1;    /*!< Allocated LVGL buffer will be DMA capable */
+        unsigned int buff_spiram: 1; /*!< Allocated LVGL buffer will be in PSRAM */
+    } flags;
+} bsp_display_cfg_t;
+
+/**
+ * @brief Initialize display
+ *
+ * This function initializes SPI, display controller and starts LVGL handling task.
+ *
+ * @return Pointer to LVGL display or NULL when error occurred
+ */
+lv_display_t *bsp_display_start(void);
+
+/**
+ * @brief Initialize display
+ *
+ * This function initializes SPI, display controller and starts LVGL handling task.
+ * LCD backlight must be enabled separately by calling bsp_display_brightness_set()
+ *
+ * @param cfg display configuration
+ *
+ * @return Pointer to LVGL display or NULL when error occurred
+ */
+lv_display_t *bsp_display_start_with_config(const bsp_display_cfg_t *cfg);
+
+/**
+ * @brief Get pointer to input device (touch, buttons, ...)
+ *
+ * @note The LVGL input device is initialized in bsp_display_start() function.
+ *
+ * @return Pointer to LVGL input device or NULL when not initialized
+ */
+lv_indev_t *bsp_display_get_input_dev(void);
+
+/**
+ * @brief Take LVGL mutex
+ *
+ * @param timeout_ms Timeout in [ms]. 0 will block indefinitely.
+ * @return true  Mutex was taken
+ * @return false Mutex was NOT taken
+ */
+bool bsp_display_lock(uint32_t timeout_ms);
+
+/**
+ * @brief Give LVGL mutex
+ *
+ */
+void bsp_display_unlock(void);
+
+/**
+ * @brief Rotate screen
+ *
+ * Display must be already initialized by calling bsp_display_start()
+ *
+ * @param[in] disp Pointer to LVGL display
+ * @param[in] rotation Angle of the display rotation
+ */
+void bsp_display_rotate(lv_display_t *disp, lv_disp_rotation_t rotation);
+#endif // BSP_CONFIG_NO_GRAPHIC_LIB == 0
+
+
+
+/**************************************************************************************************
+ *
+ * ADC interface
+ *
+ * There are multiple devices connected to ADC peripheral:
+ *  - Buttons
+ *
+ * After initialization of ADC, use adc_handle when using ADC driver.
+ **************************************************************************************************/
+
+#define BSP_ADC_UNIT     ADC_UNIT_1
+
+/**
+ * @brief Initialize ADC
+ *
+ * The ADC can be initialized inside BSP, when needed.
+ *
+ * @param[out] adc_handle Returned ADC handle
+ */
+esp_err_t bsp_adc_initialize(void);
+
+
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
+/**
+ * @brief Get ADC handle
+ *
+ * @note This function is available only in IDF5 and higher
+ *
+ * @return ADC handle
+ */
+adc_oneshot_unit_handle_t bsp_adc_get_handle(void);
+#endif
+
+#ifdef __cplusplus
+}
+#endif
